4652be626cc1de06de52b4803719765c5cb755c5
mingtiancai <2427373908@qq.com>
2022-01-28 02:25:46 +0800

    添加header

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-go build -o ./target/server main.go
+# go build -o ./target/server main.go
 # go build -o ./target/chitchat/chitchat ./ChitChat/*.go
-go build -o ./target/c3/handle ./c3/handle/*.go
-go build -o ./target/c3/handlefunc ./c3/handlefunc/*.go
-go build -o ./target/c3/chain_handlefunc ./c3/chain_handlefunc/*.go
-go build -o ./target/c3/chain_handler ./c3/chain_handler/*.go
-go build -o ./target/c3/httprouter ./c3/httprouter/*.go
-go build -o ./target/c3/https ./c3/https/*.go
+# go build -o ./target/c3/handle ./c3/handle/*.go
+# go build -o ./target/c3/handlefunc ./c3/handlefunc/*.go
+# go build -o ./target/c3/chain_handlefunc ./c3/chain_handlefunc/*.go
+# go build -o ./target/c3/chain_handler ./c3/chain_handler/*.go
+# go build -o ./target/c3/httprouter ./c3/httprouter/*.go
+# go build -o ./target/c3/https ./c3/https/*.go
+go build -o ./target/c4/header ./c4/header/*.go
