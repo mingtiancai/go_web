2520d1ac84b634f89c50746d3ec4ad4ca3755f06
mingtiancai <2427373908@qq.com>
2022-01-28 01:12:02 +0800

    add https但是服务跑不起来

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ go build -o ./target/c3/handlefunc ./c3/handlefunc/*.go
 go build -o ./target/c3/chain_handlefunc ./c3/chain_handlefunc/*.go
 go build -o ./target/c3/chain_handler ./c3/chain_handler/*.go
 go build -o ./target/c3/httprouter ./c3/httprouter/*.go
+go build -o ./target/c3/https ./c3/https/*.go
---
+++ c3/https/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.16
+
+require golang.org/x/net v0.0.0-20220127074510-2fabfed7e28f // indirect
