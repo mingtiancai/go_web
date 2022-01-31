209856a83a0636860b6df22c2b546cdc2142c1b0
mingtiancai <2427373908@qq.com>
2022-01-31 20:30:38 +0800

    add gorm

--- build.sh
+++ build.sh
@@ -24,4 +24,5 @@
 # go build -o ./target/c6/gob_store/ ./c6/gob_store/*.go
 # go build -o ./target/c6/sql_store1/ ./c6/sql_store1/*.go
 # go build -o ./target/c6/sql_store2/ ./c6/sql_store2/*.go
-go build -o ./target/c6/sqlx/ ./c6/sqlx/*.go
+# go build -o ./target/c6/sqlx/ ./c6/sqlx/*.go
+go build -o ./target/c6/gorm/ ./c6/gorm/*.go
