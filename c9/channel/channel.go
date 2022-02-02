7acfb4b63c6fe7f06964e990230d176a6c8c6f45
mingtiancai <2427373908@qq.com>
2022-02-02 13:11:50 +0800

    add sendRecMessage

--- c9/channel/channel.go
+++ c9/channel/channel.go
@@ -21,10 +21,36 @@ func printLetters2(w chan bool) {
 	w <- true
 }
 
-func main() {
+func useChannel() {
 	w1, w2 := make(chan bool), make(chan bool)
 	go printNumber2(w1)
 	go printLetters2(w2)
 	<-w1
 	<-w2
+}
+
+func thrower(c chan int) {
+	for i := 0; i < 5; i++ {
+		c <- i
+		fmt.Println("Threw >> ", i)
+	}
+}
+
+func catcher(c chan int) {
+	for i := 0; i < 5; i++ {
+		num := <-c
+		fmt.Println("Caught << ", num)
+	}
+}
+
+func sendRecMessage() {
+	c := make(chan int, 3)
+	go thrower(c)
+	go catcher(c)
+	time.Sleep(1000 * time.Microsecond)
+}
+
+func main() {
+	// useChannel()
+	sendRecMessage()
 }
