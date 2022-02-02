89899200e4678f477b47efa31d3c15d1f54fdfcf
mingtiancai <2427373908@qq.com>
2022-02-02 17:44:55 +0800

    add useSelect2

--- c9/channel/channel.go
+++ c9/channel/channel.go
@@ -75,8 +75,40 @@ func useSelect() {
 	}
 }
 
+func callerA2(c chan string) {
+	c <- "Hello World!"
+	close(c)
+}
+
+func callerB2(c chan string) {
+	c <- "Hola Mundo!"
+	close(c)
+}
+
+func useSelect2() {
+	a, b := make(chan string), make(chan string)
+	go callerA2(a)
+	go callerB2(b)
+	ok1, ok2 := true, true
+	var msg string
+
+	for ok1 || ok2 {
+		select {
+		case msg, ok1 = <-a:
+			if ok1 {
+				fmt.Printf("%s from A\n", msg)
+			}
+		case msg, ok2 = <-b:
+			if ok2 {
+				fmt.Printf("%s from B\n", msg)
+			}
+		}
+	}
+}
+
 func main() {
 	// useChannel()
 	// sendRecMessage()
-	useSelect()
+	// useSelect()
+	useSelect2()
 }
