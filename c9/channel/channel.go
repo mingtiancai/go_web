681e7e675c2cf6c91fff0ac00607eed09e9243c5
mingtiancai <2427373908@qq.com>
2022-02-02 17:30:55 +0800

    add use_select

--- c9/channel/channel.go
+++ c9/channel/channel.go
@@ -50,7 +50,33 @@ func sendRecMessage() {
 	time.Sleep(1000 * time.Microsecond)
 }
 
+func callerA(c chan string) {
+	c <- "Hello World!"
+}
+
+func callerB(c chan string) {
+	c <- "Hola Mundo!"
+}
+
+func useSelect() {
+	a, b := make(chan string), make(chan string)
+	go callerA(a)
+	go callerB(b)
+
+	for i := 0; i < 5; i++ {
+		select {
+		case msg := <-a:
+			fmt.Printf("%s from A\n", msg)
+		case msg := <-b:
+			fmt.Printf("%s from B\n", msg)
+		default:
+			fmt.Println("Default")
+		}
+	}
+}
+
 func main() {
 	// useChannel()
-	sendRecMessage()
+	// sendRecMessage()
+	useSelect()
 }
